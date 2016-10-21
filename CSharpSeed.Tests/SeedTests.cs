de936241ae41430a657572c8cf672e9be8ebffd4
GrosBibi <aubineau.romain@gmail.com>
2016-10-21 15:30:13 +0200

    first commit

---
+++ CSharpSeed.Tests/SeedTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using CSharpSeed;
+
+namespace CSharpSeed.Tests
+{
+  [TestFixture()]
+	public class SeedTests
+	{
+		[Test()]
+		public void TestTheTruth()
+		{
+      var seed = new Seed();
+			Assert.IsTrue(seed.ReturnTheTruth());
+		}
+	}
+}
